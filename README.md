6462ba6ed599212ab524e28ce2f8314900ee6fa0
LAPTOP-MBO56LT1\nhiph <phunghuynhnhi@797@gmail.com>
2022-04-01 17:56:38 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+npm i async-request: like axios, fetch
+npm i express : server side render, create web html in serve and res web # client side, create data and res data for client use in framework
+
+static file: include files in server (img, css, js, docs) usually in public
+=> config to access static file
+
+npm install hbs: like jsx (extension *.jsx), write js in html easily (extension *.hbs), or pug
---
+++ app/index.js
@@ -0,0 +1,58 @@
+const asyncRequest = require('async-request')
+const express = require('express')
+const app = express() // init application
+const path = require('path') // build in nodejs
+
+
+const ACCESS_KEY = 'f96c043608ecad05fce89b5a117707b5'
+const port = 7000 
+const pathPublic = path.join(__dirname, "../public") // __dirname la dg dan tuyet doi toi file dang run (app/index.js), ket hop param 2 de di den thu muc public de set staticfile pathPublic
+app.use(express.static(pathPublic)) // application will use pathPublic to link to public
+//  => http://localhost:7000/image/husky.jpg  server file will link to public folder
+
+const getWeater = async (region) => {
+    const url = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${region}`
+    try {
+        const res = await asyncRequest(url)
+        let data = {}
+        if(res) data = JSON.parse(res.body)
+        const weather = { 
+            region: data.location.region,
+            country: data.location.country,
+            temperature: data.current.temperature,
+            windSpeed: data.current.wind_speed,
+            precip: data.current.precip,
+            cloudcover: data.current.cloudcover,
+            humidity: data.current.humidity
+        }
+        console.log('weather', weather);
+        return weather
+    } catch(e) {
+        console.log('Not found', e);
+    }
+}
+// set hbs view
+app.set("view engine", "hbs") 
+
+app.get('/', async (req, res) => {
+    const location = req.query.address
+    const weather = await getWeater(location)
+    console.log('weather', weather);
+
+    // res.send("Hello world") return string
+
+    // return webpage with param 2 use for html hbs
+    res.render("weather", {
+        region: weather.region,
+        country: weather.country,
+        temperature: weather.temperature,
+        windSpeed: weather.windSpeed,
+        precip: weather.precip,
+        cloudcover: weather.cloudcover,
+        humidity: weather.humidity
+    }) 
+})
+
+app.listen(port , () => {
+    console.log(`port ${port} running`);
+})
